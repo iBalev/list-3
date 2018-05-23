a4400bd9f7f77d4113d06f36e25fa6e3a0c5e116
iBalev <ibalev@gmail.com>
2018-05-23 20:51:18 +0200

    prv komit

---
+++ src/Pocetna.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pocetna {
+
+	public static void main(String[] args) {
+		List<String> iminja;
+		iminja=new ArrayList<>();
+		iminja.add("Johntra");
+		iminja.add("Alpa");
+		iminja.add("Seanco");
+		System.out.println(iminja);
+		System.out.println(iminja.size());
+
+	}
+
+}
